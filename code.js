2ee99c7bee3a74fdb0b42b941c81426f623c53e8
jeff <jefferykibet@gmail.com>
2026-07-21 16:07:33 +0300

    fix: correct typo in getElementById and improve event listener functionality

--- code.js
+++ code.js
@@ -1,3 +1,15 @@
 const form = document.getElementById("searchForm")
 const wordInput = document.getElementById("wordInput")
 const result = document.getElemenrById("result")
+
+form.addEventListener("submit", function(event) {
+event.preventDefault();
+
+const word = wordInput.ariaValueMax.trim();
+
+if(word === "") {
+    result.innerHTML ="<p class='error'>Please enter a word.</p>";
+    return;
+}
+    searchWord(word)
+})
